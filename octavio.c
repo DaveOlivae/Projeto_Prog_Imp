6217b3190f56c8db41efc9f72e963dcc7c1209ad
mercenaryjohn <160885458+mercenaryjohn@users.noreply.github.com>
2024-11-24 12:58:18 -0300

    Update octavio.c

--- octavio.c
+++ octavio.c
@@ -325,7 +325,7 @@ void TrocarLouO (char Data_T[], char Sala_T[], char Hora_T[])
 	FILE *Ponteiro_Arquivo; // Aponta para um arquivo
 	char ID_do_arquivo[Size_nome]; // Array que recebe o nome do Arquivo, formato exemplo: 112024
     
-	ID_do_arquivo = Data_T[]; //input do usuario para ID_do_arquivo
+	strcpy(ID_do_arquivo, Data_T); //input do usuario para ID_do_arquivo
 
 	ID_do_arquivo[Size_nome - 4] = '.';
 	ID_do_arquivo[Size_nome - 3] = 'c';
@@ -359,7 +359,7 @@ void TrocarLouO (char Data_T[], char Sala_T[], char Hora_T[])
 ////// Parte de modifica horários ###########################################################
 
     char nova_sala[Size_nome]; // SALA ##############################
-    nova_sala = Sala_T[];
+	strcpy(nova_sala, Sala_T);
 	for (int i = 0; i < strlen(nova_sala); i++)
 	{
         if (nova_sala[i] == '\n')
@@ -370,7 +370,7 @@ void TrocarLouO (char Data_T[], char Sala_T[], char Hora_T[])
     
 	int bloco_horario; // HORARIO ##############################
     // Converte a string para um int
-    bloco_horario = atoi(Hora_T[]);
+	strcpy(bloco_horario, atoi(Hora_T));
 	// Calcular os intervalos de 50 minutos
     bloco_horario = ((bloco_horario - 710) / 50) * 2; // Começa a contar a partir do 0, *2 por conta do ';'
 	//printf("_1BLOH_%i_\n", bloco_horario);
@@ -478,7 +478,7 @@ void AdicionarSala (char Sala_T)
 ////// Parte de adicionar salas
 
 	char nova_sala[Size_nome]; // SALA ##############################
-	nova_sala = Sala_T[];
+	strcpy(nova_sala, Sala_T);
 	for (int i = 0; i < strlen(nova_sala); i++)
 	{
         	if (nova_sala[i] == '\n')
